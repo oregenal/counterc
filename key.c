c0b53994820bd21cf69c3f7d28b47a3f5aa849fd
Andrey Gud <nosacred@gmail.com>
2024-08-21 18:41:07 +0700

    minor refactoring

--- key.c
+++ key.c
@@ -6,42 +6,45 @@
 #include <string.h>
 #include <stdio.h>
 
+#define KEYBOARD "/dev/input/by-path/pci-0000:00:14.0-usb-0:7:1.0-event-kbd"
+
 static const char *const evval[3] = {
-    "RELEASED",
-    "PRESSED ",
-    "REPEATED"
+	"RELEASED",
+	"PRESSED ",
+	"REPEATED"
 };
 
 int main(void)
 {
-    const char *dev = "/dev/input/by-path/pci-0000:00:14.0-usb-0:7:1.0-event-kbd";
-    struct input_event ev;
-    ssize_t n;
-    int fd;
+	const char *dev = KEYBOARD;
+	struct input_event ev;
+	ssize_t n;
+	int fd;
 
-    fd = open(dev, O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
-        return EXIT_FAILURE;
-    }
+	fd = open(dev, O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
+		return EXIT_FAILURE;
+	}
 
-    while (1) {
-        n = read(fd, &ev, sizeof ev);
-        if (n == (ssize_t)-1) {
-            if (errno == EINTR)
-                continue;
-            else
-                break;
-        } else
-        if (n != sizeof ev) {
-            errno = EIO;
-            break;
-        }
+	while (1) {
+		n = read(fd, &ev, sizeof ev);
+		if (n == (ssize_t)-1) {
+			if (errno == EINTR)
+				continue;
+			else
+				break;
+		} else
+			if (n != sizeof ev) {
+				errno = EIO;
+				break;
+			}
 
-        if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2)
-            printf("%s 0x%04x (%d)\n", evval[ev.value], (int)ev.code, (int)ev.code);
-    }
-    fflush(stdout);
-    fprintf(stderr, "%s.\n", strerror(errno));
-    return EXIT_FAILURE;
+		if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2)
+			printf("%s 0x%04x (%d)\n", evval[ev.value], (int)ev.code, (int)ev.code);
+	}
+
+	fflush(stdout);
+	fprintf(stderr, "%s.\n", strerror(errno));
+	return EXIT_FAILURE;
 }
